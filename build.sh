69d515ebc02c16d818b45d6a88960714cbffb9f9
Azriel Hoh <azriel91@gmail.com>
2025-08-16 14:47:01 +1200

    Add pages.yml github workflow.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+set -euo pipefail
+
+mdslides --mdbook-path . \
+  --output-dir ./html \
+  --template template.html \
+  --index-template index_template.html
+
+for ext in svg png
+do
+  find src/ -type f -name "*.${ext}" \
+    | cut -d '/' -f 2- \
+    | xargs -I {} bash -c 'mkdir -p ./html/$(dirname {}); cp src/{} ./html/{}'
+done
+
+cp -R src/js ./html
+cp -R src/css ./html
